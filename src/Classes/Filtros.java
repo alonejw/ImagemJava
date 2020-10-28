d0ca7d0128db11397ebcafb5bbe6a33ab0c0a260
fwmar <fwmar@DESKTOP-EOMIMG0>
2020-10-28 19:50:19 -0300

    oi

--- src/Classes/Filtros.java
+++ src/Classes/Filtros.java
@@ -24,8 +24,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
-//ghj
-//oioi
+
 /**
  *
  * @author fwmar
